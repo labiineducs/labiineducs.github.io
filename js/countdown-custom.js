e6a7dd8c9f356d47150d08a50d24faa79d439488
Pancho <pancho@dc.exa.unrc.edu.ar>
2024-10-13 23:36:27 -0300

    .

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,7 +1,7 @@
 (function($) {	
 	'use strict';
         jQuery(function () {
-            jQuery('#defaultCountdown').countdown({until: new Date(2024, 10-1, 21, 18)}); // year, month, date, hour
+            jQuery('#defaultCountdown').countdown({until: new Date(2024, 10-1, 21, 18)});  //  year, month, date, hour
         });
 })(jQuery);
 
