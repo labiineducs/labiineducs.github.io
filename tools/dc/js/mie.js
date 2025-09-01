3817c66247e1a0127e029b89c3691e89fa4fcf1e
Pancho <pancho@dc.exa.unrc.edu.ar>
2025-09-01 01:06:26 -0300

    .

--- tools/dc/js/mie.js
+++ tools/dc/js/mie.js
@@ -458,6 +458,7 @@ let completionsMD = [
 	{ name: 'posicionY', caption: 'posicionY()', value: 'posicionY()', meta: 'function', type: "sensor DC", score: 1000}
 ];
 
+
 mie.lang.p5.functionNames = [
 	'preload',
 	'setup',
