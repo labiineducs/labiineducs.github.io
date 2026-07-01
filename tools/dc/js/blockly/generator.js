7d7601e8bfb8c3a293803e62402e326061d658b2
BenjaminBavera <benjabavera@gmail.com>
2026-07-01 17:17:38 -0300

    Mejora visual de bloques y paleta de colores interactiva

--- tools/dc/js/blockly/generator.js
+++ tools/dc/js/blockly/generator.js
@@ -20,8 +20,21 @@
 
         //Bloques de Color
         javascript.javascriptGenerator.forBlock['bloque_seleccionarColor'] = function(block) {
-            const colorElegido = block.getFieldValue('COLOR');
-            return 'seleccionarColor("' + colorElegido + '");\n';
+            const hexElegido = block.getFieldValue('COLOR');
+            // Diccionario para traducir de vuelta a los nombres en inglés 
+            const hexANombre = {
+                '#000000': 'black', '#808080': 'gray', '#c0c0c0': 'silver', '#ffffff': 'white',
+                '#d2b48c': 'tan', '#a0522d': 'sienna', '#d2691e': 'chocolate', '#a52a2a': 'brown',
+                '#800000': 'maroon', '#ff0000': 'red', '#ff1493': 'deeppink', '#ffc0cb': 'pink',
+                '#ff7f50': 'coral', '#ffa500': 'orange', '#ffd700': 'gold', '#ffff00': 'yellow',
+                '#9acd32': 'yellowgreen', '#00ff00': 'lime', '#008000': 'green', '#228b22': 'forestgreen',
+                '#808000': 'olive', '#008080': 'teal', '#00ffff': 'cyan', '#40e0d0': 'turquoise',
+                '#87ceeb': 'skyblue', '#4169e1': 'royalblue', '#0000ff': 'blue', '#000080': 'navy',
+                '#4b0082': 'indigo', '#800080': 'purple', '#ff00ff': 'magenta', '#ee82ee': 'violet'
+            }; 
+            const colorNombre = hexANombre[hexElegido] || hexElegido;
+
+            return 'seleccionarColor("' + colorNombre + '");\n';
         };
         javascript.javascriptGenerator.forBlock['bloque_siguienteColor'] = function(block){
             return 'siguienteColor();\n';
@@ -29,6 +42,21 @@
         javascript.javascriptGenerator.forBlock['bloque_colorRandom'] = function(block){
             return 'colorRandom();\n';
         }
+        javascript.javascriptGenerator.forBlock['bloque_colores'] = function(block) {
+            const hexElegido = block.getFieldValue('COLOR');
+            const hexANombre = {
+                '#000000': 'black', '#808080': 'gray', '#c0c0c0': 'silver', '#ffffff': 'white',
+                '#d2b48c': 'tan', '#a0522d': 'sienna', '#d2691e': 'chocolate', '#a52a2a': 'brown',
+                '#800000': 'maroon', '#ff0000': 'red', '#ff1493': 'deeppink', '#ffc0cb': 'pink',
+                '#ff7f50': 'coral', '#ffa500': 'orange', '#ffd700': 'gold', '#ffff00': 'yellow',
+                '#9acd32': 'yellowgreen', '#00ff00': 'lime', '#008000': 'green', '#228b22': 'forestgreen',
+                '#808000': 'olive', '#008080': 'teal', '#00ffff': 'cyan', '#40e0d0': 'turquoise',
+                '#87ceeb': 'skyblue', '#4169e1': 'royalblue', '#0000ff': 'blue', '#000080': 'navy',
+                '#4b0082': 'indigo', '#800080': 'purple', '#ff00ff': 'magenta', '#ee82ee': 'violet'
+            };
+            const colorNombre = hexANombre[hexElegido] || hexElegido; 
+            return ['"' + colorNombre + '"', javascript.javascriptGenerator.ORDER_ATOMIC];
+        };
 
         //Bloques de Sensor (Devuelven un valor, no usan salto de línea \n)
         javascript.javascriptGenerator.forBlock['bloque_estaPintado'] = function(block) {
